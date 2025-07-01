ebb6449892721435f1c72beb26ed6bbd62a99076
AlanDurdevic <alan.durdevic@fer.hr>
2025-07-01 18:44:07 +0200

    Setting up project

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="TicketHub", description="Ticket Hub application", version="0.0.1")
+
+
+@app.get(
+    "/"
+)
+async def get_tickets():
+    return "Hello from TicketHub"
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", port=8080, reload=True)
