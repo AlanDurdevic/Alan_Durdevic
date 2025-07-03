1bf45ab2c688be398281ebbe4aa8b5b66fed5769
AlanDurdevic <alan.durdevic@fer.hr>
2025-07-03 13:53:43 +0200

    Added ticket model

--- src/models.py
+++ src/models.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel
-
+from typing import Literal, Optional
 
 class User(BaseModel):
     """User model"""
     id: int
     username: str
+
+
+class Ticket(BaseModel):
+    """Ticket model"""
+    id: int
+    title: str
+    status: Literal["open", "closed"]
+    priority: Literal["low", "medium", "high"]
+    assignee: Optional[str] = None
