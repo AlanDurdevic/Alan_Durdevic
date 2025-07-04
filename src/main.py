124ca6087f7a8f50133aec068e4e1fb8b79ac809
AlanDurdevic <alan.durdevic@fer.hr>
2025-07-04 09:35:23 +0200

    Added rate limiting with slowapi

--- src/main.py
+++ src/main.py
@@ -4,8 +4,15 @@ from typing import Optional, Literal
 from models import PaginatedResponse, Ticket, TicketStats
 from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 import logging
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 
 app = FastAPI(title="TicketHub", description="TicketHub application for Abysalto AI Academy ", version="0.0.1")
+app.state.limiter = Limiter(key_func=get_remote_address, default_limits=["5/minute"])
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 app_service = Service()
 
